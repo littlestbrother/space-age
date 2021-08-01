b5daa6f1f14da1e6896cda89ac57a4d5c62fdad6
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-31 19:50:39 -0700

    test failed

--- __tests__/logic.test.js
+++ __tests__/logic.test.js
@@ -2,8 +2,10 @@ import Date from '../src/logic.js'
 
 describe('Object "Date" creation', () => {
     test('should create Date class', () => {
-        let newDate = new Date(03,21,2002);
+        let newDate = new Date(3,21,2002);
         console.log(newDate);
-        expect(newDate.name).toEqual(03,21,0021);
+        expect(newDate.month).toEqual(3);
+        expect(newDate.day).toEqual(21);
+        expect(newDate.year).toEqual(21);
     });
   });
