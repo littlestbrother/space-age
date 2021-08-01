e0d2414f8f66e06f345a0cf4065e342b385f3e4c
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-31 19:38:00 -0700

    describe Date

---
+++ __tests__/mult.test.js
@@ -0,0 +1,6 @@
+describe('Object "Date" creation', () => {
+    test('should create Date class', () => {
+        let newDate = new Date(03,21,2002);
+        expect(character.name).toEqual(03,21,0021);
+    });
+  });
