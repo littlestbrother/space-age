408df464efd40b7e30af73e308b4e4780b5588b4
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-08-04 21:37:53 -0700

    add more tests

--- __tests__/logic.test.js
+++ __tests__/logic.test.js
@@ -1,35 +1,55 @@
-import { Person } from '../src/logic.js'
+import Person from '../src/logic.js'
 
-describe('Object "Person" creation', () => {
-    test('should create Person object', () => {
-        let newPerson = new Person(19,'male',true);
-        expect(newPerson.age).toEqual(19);
-        expect(newPerson.gender).toEqual('male');
-        expect(newPerson.helmet).toEqual(true);
+describe('Object male-helmet "Person" creation', () => {
+  test('should create Person object', () => {
+    let newPerson = new Person(19, 'male', true);
+    expect(newPerson.age).toEqual(19);
+    expect(newPerson.gender).toEqual('male');
+    expect(newPerson.helmet).toEqual(true);
 
-    });
   });
+});
 
-  describe('getLifeExp()', () => {
-    test('should calculate users life expectancy', () => {
-        let newPerson = new Person(19,'male',true);
-        newPerson.getLifeExp();
-        expect(newPerson.lifeExp).toEqual(57);
-    });
-  });
+describe('Object female-helmet "Person" creation', () => {
+  test('should create Person object', () => {
+    let newPerson = new Person(21, 'female', true);
+    expect(newPerson.age).toEqual(21);
+    expect(newPerson.gender).toEqual('female');
+    expect(newPerson.helmet).toEqual(true);
 
-  describe('calc()', () => {
-    test('should calculate append the users age and life expectency multiplied by planets multiplier', () => {
-        let newPerson = new Person(19,'male',true);
-        newPerson.getLifeExp();
-        newPerson.calc();
-        expect(newPerson.mercuryAge).toEqual(4.56);
-        expect(newPerson.mercuryExp).toEqual(13.68);
-        expect(newPerson.venusAge).toEqual(11.78);
-        expect(newPerson.venusExp).toEqual(35.34);
-        expect(newPerson.marsAge).toEqual(35.72);
-        expect(newPerson.marsExp).toEqual(107.16);
-        expect(newPerson.jupiterAge).toEqual(225.34);
-        expect(newPerson.jupiterExp).toEqual(676.02);
-    });
   });
+});
+
+describe('Object horse-no-helmet "Person" creation', () => {
+  test('should create Person object', () => {
+    let newPerson = new Person(34, 'horse', false);
+    expect(newPerson.age).toEqual(34);
+    expect(newPerson.gender).toEqual('horse');
+    expect(newPerson.helmet).toEqual(false);
+
+  });
+});
+
+describe('getLifeExp()', () => {
+  test('should calculate users life expectancy', () => {
+    let newPerson = new Person(19, 'male', true);
+    newPerson.getLifeExp();
+    expect(newPerson.lifeExp).toEqual(57);
+  });
+});
+
+describe('calc()', () => {
+  test('should calculate append the users age and life expectency multiplied by planets multiplier', () => {
+    let newPerson = new Person(19, 'male', true);
+    newPerson.getLifeExp();
+    newPerson.calc();
+    expect(newPerson.mercuryAge).toEqual(4.56);
+    expect(newPerson.mercuryExp).toEqual(13.68);
+    expect(newPerson.venusAge).toEqual(11.78);
+    expect(newPerson.venusExp).toEqual(35.34);
+    expect(newPerson.marsAge).toEqual(35.72);
+    expect(newPerson.marsExp).toEqual(107.16);
+    expect(newPerson.jupiterAge).toEqual(225.34);
+    expect(newPerson.jupiterExp).toEqual(676.02);
+  });
+});
